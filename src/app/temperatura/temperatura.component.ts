3994d6eed5ebfa48990a859d5881b14c1592dacd
DIPUJAEN\DJ11747G <flledo@dipujaen.es>
2023-11-24 11:28:28 +0100

    - Actualizacion de node - Un par de nuevos tipos de cielo

--- src/app/temperatura/temperatura.component.ts
+++ src/app/temperatura/temperatura.component.ts
@@ -2,18 +2,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { HttpService } from '../servicios/http.service';
 
 export enum EstadoCielo {
-  Soleado='Despejado',
-  PocoNuboso='Poco nuboso',
-  Nuboso='Nuboso',
-  MuyNuboso='Muy nuboso',
-  NubesAltas='Nubes altas',
-  Cubierto='Cubierto',
-  PocoLluvioso='Cubierto con lluvia escasa',
-  Lluvioso='Lluvioso',
-  MuyLluvioso='Muy lluvioso',
-  Tormenta='Tormenta',
-  Nieve='Nieve',
-  Desconocido=''
+  Soleado = 'Despejado',
+  IntervalosNubosos = 'Intervalos nubosos',
+  PocoNuboso = 'Poco nuboso',
+  Nuboso = 'Nuboso',
+  MuyNuboso = 'Muy nuboso',
+  NubesAltas = 'Nubes altas',
+  Cubierto = 'Cubierto',
+  Niebla = 'Niebla',
+  PocoLluvioso = 'Cubierto con lluvia escasa',
+  Lluvioso = 'Lluvioso',
+  MuyLluvioso = 'Muy lluvioso',
+  Tormenta = 'Tormenta',
+  Nieve = 'Nieve',
+  Desconocido = ''
 }
 
 @Component({
@@ -22,39 +24,37 @@ export enum EstadoCielo {
   styleUrls: ['./temperatura.component.css']
 })
 export class TemperaturaComponent {
-    @Input() nombre: string = "";
-    @Input() temperatura: number | string = "Cargando";
-    @Input() tempMax: number | string = "Cargando";
-    @Input() tempMin: number | string = "Información"
-    @Input() cielo: EstadoCielo=EstadoCielo.Desconocido;
-    @Input() codigo: string="";
+  @Input() nombre: string = "";
+  @Input() temperatura: number | string = "Cargando";
+  @Input() tempMax: number | string = "Cargando";
+  @Input() tempMin: number | string = "Información"
+  @Input() cielo: EstadoCielo = EstadoCielo.Desconocido;
+  @Input() codigo: string = "";
 
-    @Output() temperaturaChange = new EventEmitter<number>();
+  @Output() temperaturaChange = new EventEmitter<number>();
 
-    EstadoCielo = EstadoCielo;
-    
-    constructor (private Http: HttpService) {}
+  EstadoCielo = EstadoCielo;
 
-    ngOnInit()
-    {
-      this.Http.getTemperaturas(this.codigo)
-        .subscribe(data => 
-        {
-          let xml = JSON.parse(JSON.stringify(data));
-          this.temperatura = xml.temperatura_actual;
-          this.tempMax = xml.temperaturas.max;
-          this.tempMin = xml.temperaturas.min;
-          this.cielo = xml.stateSky.description;
-          //console.log (xml.stateSky.description);
-        });
-    }
-    
-    /*incrementar():void{
-        this.temperatura++;
-        this.temperaturaChange.emit(this.temperatura);
-    }
-    decrementar():void{
-        this.temperatura--;
-        this.temperaturaChange.emit(this.temperatura);
-    }*/
+  constructor(private Http: HttpService) { }
+
+  ngOnInit() {
+    this.Http.getTemperaturas(this.codigo)
+      .subscribe(data => {
+        let xml = JSON.parse(JSON.stringify(data));
+        this.temperatura = xml.temperatura_actual;
+        this.tempMax = xml.temperaturas.max;
+        this.tempMin = xml.temperaturas.min;
+        this.cielo = xml.stateSky.description;
+        //console.log (xml.stateSky.description);
+      });
+  }
+
+  /*incrementar():void{
+      this.temperatura++;
+      this.temperaturaChange.emit(this.temperatura);
+  }
+  decrementar():void{
+      this.temperatura--;
+      this.temperaturaChange.emit(this.temperatura);
+  }*/
 }
